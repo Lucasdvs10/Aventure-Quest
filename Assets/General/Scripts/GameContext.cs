ea5a77645bd9caac28864b292c9c993e1efa34fc
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2026-04-24 19:26:31 -0300

    Add Player on gamecontext

--- Assets/General/Scripts/GameContext.cs
+++ Assets/General/Scripts/GameContext.cs
@@ -3,12 +3,17 @@ using UnityEngine;
 public class GameContext : MonoBehaviour
 {
     public SOGameProperties SOGameProperties;
+    public GameObject PlayerGameObject;
 
     public static SOGameProperties GameProperties { get; private set; }
+    public static GameObject PlayerGameObjectInstance { get; private set; }
 
     void Awake()
     {
         Debug.Assert(SOGameProperties != null, "O SO do gameproperties está nulo no game context!", this);
         GameProperties = SOGameProperties;
+
+        Debug.Assert(PlayerGameObjectInstance != null, "O Player GameObject está nulo no game context!", this);
+        PlayerGameObjectInstance = PlayerGameObject;
     }
 }
