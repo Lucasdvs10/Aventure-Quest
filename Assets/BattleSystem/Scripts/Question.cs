7d88918c9a87b111352a189dc8382fd22e241809
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2026-04-30 21:40:26 -0300

    Add quastion class

---
+++ Assets/BattleSystem/Scripts/Question.cs
@@ -0,0 +1,12 @@
+
+public class Question
+{
+    public string Statement;
+    public string StatementA;
+    public string StatementB;
+    public string StatementC;
+    public string StatementD;
+
+    public string CorrectOption;
+    
+}
