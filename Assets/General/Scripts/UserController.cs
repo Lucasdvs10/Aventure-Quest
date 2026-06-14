a68b054c4b35ec61e9baf740b016364b9991fb6a
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2026-06-14 13:47:31 -0300

    Login screen WIP

---
+++ Assets/General/Scripts/UserController.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class UserController
+{
+    private const string BaseUrl = "https://dungeon-quest-api.fly.dev/api";
+    private string LoginEndpoint = $"{BaseUrl}/users/login";
+
+    private static readonly HttpClient HttpClient = new();
+
+
+    public async Task<bool> LoginAsync(string userName, string password)
+    {
+        try
+        {
+            LoginRequest request = new()
+            {
+                user_name = userName,
+                password = password
+            };
+
+            string json =
+                JsonUtility.ToJson(request);
+
+            StringContent content =
+                new(
+                    json,
+                    Encoding.UTF8,
+                    "application/json"
+                );
+
+            HttpResponseMessage response =
+                await HttpClient.PostAsync(
+                    LoginEndpoint,
+                    content
+                );
+
+            if (!response.IsSuccessStatusCode)
+            {
+                Debug.LogWarning(
+                    $"Erro HTTP: {response.StatusCode}"
+                );
+
+                return false;
+            }
+
+            string responseJson =
+                await response.Content.ReadAsStringAsync();
+
+            LoginResponse loginResponse =
+                JsonUtility.FromJson<LoginResponse>(
+                    responseJson
+                );
+
+            return loginResponse?.response?.valid ?? false;
+        }
+        catch (Exception e)
+        {
+            Debug.LogError(
+                $"Erro ao realizar login: {e}"
+            );
+
+            return false;
+        }
+    }
+
+    [Serializable]
+    private class LoginRequest
+    {
+        public string user_name;
+        public string password;
+    }
+
+    [Serializable]
+    private class LoginResponse
+    {
+        public LoginResult response;
+    }
+
+    [Serializable]
+    private class LoginResult
+    {
+        public bool valid;
+    }
+}
